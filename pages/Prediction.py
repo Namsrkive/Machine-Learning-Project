107cdf13dda720f20c6a6b4bd67e4ed6313014fd
Namrata <124352728+Namsrkive@users.noreply.github.com>
2024-05-16 00:14:43 +0530

    Update Prediction.py

--- pages/Prediction.py
+++ pages/Prediction.py
@@ -2,15 +2,13 @@ import streamlit as st
 import joblib
 
 # Load the pre-trained model
-@st.cache(allow_output_mutation=True)
 def load_model():
     return joblib.load('model.pkl')
 model = load_model()
 
 import random
-
-# Function to make predictions ()
-def make_prediction(online_order, book_table, votes, approx_cost, location, cuisines):
+# Function to make predictions()
+def make_prediction(location, cuisines, online_order, book_table, votes, approx_cost):
     prediction = random.uniform(3, 5)
     return prediction
 
@@ -19,11 +17,7 @@ def main():
     st.title("Restaurant Rating Predictor")
 
     # Widgets for user input
-    online_order = st.selectbox("Online Order Facility", ["Yes", "No"])
-    book_table = st.selectbox("Book Table Facility", ["Yes", "No"])
-    votes = st.slider("Number of Votes", min_value=0, max_value=1000)
-    approx_cost = st.slider("Approx Cost for Two People", min_value=0.0, max_value=3000.0, step=0.1)
-    location = st.selectbox("Location of the Restaurant", ['Banashankari', 'Bannerghatta Road', 'Basavanagudi', 'Bellandur',
+    location=st.selectbox("City",('Banashankari', 'Bannerghatta Road', 'Basavanagudi', 'Bellandur',
                               'Brigade Road', 'Brookefield', 'BTM', 'Church Street',
                               'Electronic City', 'Frazer Town', 'HSR', 'Indiranagar',
                               'Jayanagar', 'JP Nagar', 'Kalyan Nagar', 'Kammanahalli',
@@ -31,7 +25,7 @@ def main():
                               'Koramangala 6th Block', 'Koramangala 7th Block', 'Lavelle Road',
                               'Malleshwaram', 'Marathahalli', 'MG Road', 'New BEL Road',
                               'Old Airport Road', 'Rajajinagar', 'Residency Road',
-                              'Sarjapur Road', 'Whitefield'])
+                              'Sarjapur Road', 'Whitefield'))
     cuisines = st.multiselect("Cuisines", ['Afghani', 'African', 'American', 'Andhra', 'Arabian', 'Asian',
                                                 'Assamese', 'Awadhi', 'BBQ', 'Bakery', 'Belgian', 'Bengali', 'Beverages',
                                                 'Bihari', 'Biryani', 'Bohri', 'British', 'Burmese', 'Cantonese', 'Chettinad',
@@ -43,9 +37,14 @@ def main():
                                                 'North Indian', 'Oriya', 'Parsi', 'Portuguese', 'Rajasthani', 'Russian', 'Seafood',
                                                 'Sindhi', 'Singaporean', 'South American', 'South Indian', 'Spanish', 'Sri Lankan',
                                                 'Tamil', 'Thai', 'Tibetan', 'Turkish', 'Vegan', 'Vietnamese'])
+    online_order = st.radio("Online Order Facility", ("Yes", "No"))
+    book_table = st.radio("Table Booking Facility",('Yes', 'No'))
+    votes = st.slider("Number of Votes", min_value=0, max_value=1000)
+    approx_cost = st.slider("Approx Cost for Two People", min_value=0.0, max_value=3000.0,value=100.0, step=10.0)
+    
 
     if st.button("Predict"):
-        prediction = make_prediction(online_order, book_table, votes, approx_cost, location, cuisines)
+        prediction = make_prediction(location, cuisines, online_order, book_table, votes, approx_cost)
         st.write(f"The predicted rating is: {prediction:.1f} / 5")
 
 if __name__ == '__main__':

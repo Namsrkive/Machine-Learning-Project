d9a06b3d93346478928ac302d5745f26b928a971
Namrata <124352728+Namsrkive@users.noreply.github.com>
2024-05-15 20:08:15 +0530

    Update Introduction.py

--- Introduction.py
+++ Introduction.py
@@ -5,7 +5,7 @@ from matplotlib import image
 import os
 
 st.title("Restaurant Rating Prediction")
-st.subheader("The main goal of this is to get insights on restaurants which people like visit and to identify the rating of the restaurant.")
+st.subheader("The main goal of this is to get insights on restaurants which people like to visit and to identify the rating of the restaurants in area.")
 st.write("Restaurant Rating has become the most commonly used parameter for judging a restaurant for any individual.\
           Rating of a restaurant depends on factors like reviews, area situated, average cost for two people, votes, \
           cuisines and the type of restaurant.")
